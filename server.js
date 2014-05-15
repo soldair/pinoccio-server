794cf2e8b20717164ce62224992c8396ec27bfbd
Ryan Day <soldair@gmail.com>
2014-05-15 16:21:29 -0700

    command server works!

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
-//
-//
+
+var commands = require('./index.js') 
 
 var net = require('net');
 
+var i = 0;
+
 var server = net.createServer(function(con){
-  
+  commands(con,function(err,stream){
+    console.log('connection ready!',stream.token);
+    if( i ^= 1) {
+      stream.command(1,'led.red',function(err,data){
+        console.log('led.red command callback!',err,data);
+      });
+    } else {
+      stream.command(1,'led.blue',function(err,data){
+        console.log('led.blue command callback!',err,data);
+      });
+    }
+    //setTimeout(function(){
+    //  con.end();
+    //},1000);
+  })
 });
 
-server.listen(process.env.port||processs.env.PORT||8003,function(err){
+// 22756 plain tcp, 22757 tls
+server.listen(process.env.PINOCCIO_PORT||22756,function(err){
   if(err) throw err;
   console.log('server started ',this.address());
 });
